cd90d61c74e7bc52780912e1fd8be61b5823493c
Lynn Jarvis <spout@zeal.co>
2021-01-06 10:57:44 +1030

    Add all source files
    Add all source files and VS2017 project for further work

--- Source/x64/SpoutReceiver64/VDJSpoutReceiver64.h
+++ Source/x64/SpoutReceiver64/VDJSpoutReceiver64.h
@@ -11,10 +11,11 @@
 //	d3d11.lib
 //  d3d9.lib
 
-#include "..\..\SpoutGL\SpoutSenderNames.h" // for sender creation and update
-#include "..\..\SpoutGL\SpoutDirectX.h" // for creating a shared texture
-#include "..\..\SpoutGL\SpoutFrameCount.h" // for mutex lock and new frame signal
-#include "..\..\SpoutGL\SpoutUtils.h" // for logging utilites
+#include "SpoutSDK\SpoutSenderNames.h" // for sender creation and update
+#include "SpoutSDK\SpoutDirectX.h" // for creating a shared texture
+#include "SpoutSDK\SpoutFrameCount.h" // for mutex lock and new frame signal
+#include "SpoutSDK\SpoutUtils.h" // for logging utilites
+
 #include <direct.h> // for _getcwd
 #include <TlHelp32.h> // for PROCESSENTRY32
 #include <tchar.h> // for _tcsicmp
